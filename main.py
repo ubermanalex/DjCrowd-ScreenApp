2981807677ac4976d68a73550aecc06e56756ca7
StefLB <stefanie.lehmann-berlin@web.de>
2013-06-12 17:23:11 +0200

    Hauptprogramm vom Bildschirm added

---
+++ main.py
@@ -0,0 +1,111 @@
+'''
+Created on 06.06.2013
+
+@author: Steffi
+'''
+
+from libavg import *
+import time
+import thread
+
+class screen(AVGApp):
+    def __init__(self, parentNode):
+        
+        player = avg.Player.get()  
+        global a,b,z
+        (a,b) = parentNode.size
+        canvas = player.createMainCanvas(size=(a,b)) #Aufloesung Bildschirm
+        self.rootNode = canvas.getRootNode()
+        self.back = avg.RectNode (pos=(0,0), size=(a,b), parent=self.rootNode, color="A4A4A4", fillcolor="A4A4A4", fillopacity=1) 
+        self.z = int (a-449)
+        self.title=avg.WordsNode (font="arial", variant="Bold", text="DjCrowd - Canossa", color="000000", fontsize=40, alignment="left", parent=self.rootNode)
+        print self.z
+        self.timer=avg.WordsNode (font="arial", variant="Bold", text="Song-Countdown 30:00", color="000000", fontsize=40, indent=self.z, parent=self.rootNode)
+
+        
+        def left():
+            self.divNode=avg.DivNode(pos=(0,50), size=((a/2)-20,b-50),parent=self.rootNode)
+            self.leftr=avg.RectNode (pos=(25,0), size=(((a/2)-50), b-50), parent=self.divNode, color="F0F0F0", fillopacity=1)
+            self.title=avg.WordsNode (pos=(100,0),font="arial", variant="Bold", text="Top 7 Songs", color="000000", fontsize=30, parent=self.divNode)
+            self.votes=avg.WordsNode (pos=(350,0),font="arial", variant="Bold", text="Votes", color="000000", fontsize=30, parent=self.divNode)
+            self.platz1=avg.WordsNode (pos=(70,80),font="arial", variant="Bold", text="1. ", color="DDDC3C", fontsize=30, parent=self.divNode)
+            self.platz2=avg.WordsNode (pos=(70,120),font="arial", variant="Bold", text="2. ", color="C9C9C5", fontsize=30, parent=self.divNode)
+            self.platz3=avg.WordsNode (pos=(70,160),font="arial", variant="Bold", text="3. ", color="EFBF34", fontsize=30, parent=self.divNode)
+            self.platz4=avg.WordsNode (pos=(70,200),font="arial", variant="Bold", text="4. ", color="000000", fontsize=30, parent=self.divNode)
+            self.platz5=avg.WordsNode (pos=(70,240),font="arial", variant="Bold", text="5. ", color="000000", fontsize=30, parent=self.divNode)
+            self.platz6=avg.WordsNode (pos=(70,280),font="arial", variant="Bold", text="6. ", color="000000", fontsize=30, parent=self.divNode)
+            self.platz7=avg.WordsNode (pos=(70,320),font="arial", variant="Bold", text="7. ", color="000000", fontsize=30, parent=self.divNode)
+            
+            
+        def right():
+            self.divNode=avg.DivNode(pos=(a/2,50), size=((a/2)-20,b-50),parent=self.rootNode)
+            self.rightr=avg.RectNode (pos=(0,0), size=((a/2)-20, b-50), parent=self.divNode, color="F0F0F4", fillopacity=1)
+            
+            
+        def receiveArraywithSongs():
+            
+            title=[]
+            title.append("Silbermond##Nichts passiert##7")
+            title.append("Juli##Gute Zeit##6")
+            title.append("Nickelback##Silver side up##5")
+            title.append("Carly Rae Jeapson##I just met you##4")
+            title.append("Sportfreunde Stiller##Applaus Applaus##3")
+            title.append("Will.I.am##Scream&Shout##2")
+            title.append("Justin Timberlake##Mirrors##1")
+            stringarray=[]
+
+            ArrayLen = len(title)
+            for i in range(0,ArrayLen):
+                string = title[i]
+                string2 = string.split("##")
+                stringarray.append([string2[0],string2[1],string2[2]]) ##Interpret , Titel, Votes
+            print stringarray[0][0]
+            
+            
+            self.platz1.text= "1. "+stringarray[0]
+            self.platz2.text= "2. "+stringarray[1]
+            self.platz3.text= "3. "+stringarray[2]
+            self.platz4.text= "4. "+stringarray[3]
+            self.platz5.text= "5. "+stringarray[4]
+            self.platz6.text= "6. "+stringarray[5]
+            self.platz7.text= "7. "+stringarray[6]
+            
+
+            
+            
+            
+        def countdown(m,s):
+            
+            def MsToSecs(m,s):
+                return m*60 + s
+
+            def secsToMs(secs):
+                mins = secs//60
+                secs -= mins*60
+                mins = str(mins)
+                secs = str(secs)
+                return mins,secs
+            
+            seconds = MsToSecs(m,s)
+            while seconds > 0:
+                (mint,sect)=secsToMs(seconds)
+                self.timer.text="Countdown " + mint + ":" + sect
+                seconds -= 1
+                time.sleep(1)
+                if seconds ==0:
+                    seconds = 1800
+        
+            
+        left()
+        right()
+        thread.start_new_thread(countdown,(0,2))
+        receiveArraywithSongs()
+        
+        
+        
+  
+        
+if __name__=='__main__':
+    screen.start(resolution=(1440, 900))   
+  
+
