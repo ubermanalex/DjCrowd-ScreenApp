092cba2d7f187949ae21f66edb61bb890fbc7c20
StefLB <stefanie.lehmann-berlin@web.de>
2013-06-19 14:59:40 +0200

    main mit neuem receiveArray und animation balken

--- main.py
+++ main.py
@@ -25,13 +25,14 @@ class screen(AVGApp):
             self.divNode=avg.DivNode(pos=(0,50), size=(3*(a/5),b-50),parent=self.rootNode)
             self.ranking=avg.WordsNode (pos=(45,110),font="arial", variant="Bold", width=40, height= (b-50),text="1. <br/> <br/> <br/> 2. <br/> <br/> <br/> 3. <br/> <br/> <br/> 4. <br/> <br/> <br/> 5. <br/> <br/> <br/> 6. <br/> <br/> <br/> 7.", color="000000", fontsize=30, parent=self.rootNode)
             self.leftr=avg.RectNode (pos=(0,0), size=(3*(a/5), b-50), parent=self.divNode, color="F0F0F0", fillopacity=1)
-            self.title=avg.WordsNode (pos=(75,0),font="arial", variant="Bold", text="Top 7 Songs", color="000000", fontsize=40, parent=self.divNode)
+            self.title=avg.WordsNode (pos=(75,0),font="arial", variant="Bold", text=" Top 7 Songs ", color="000000", fontsize=40, parent=self.divNode)
             self.votes=avg.WordsNode (pos=(600,0),font="arial", variant="Bold", text="Votes", color="000000", fontsize=40, parent=self.divNode)
             
             self.div1=avg.DivNode(pos=(75,110), size=(3*(a/5)-20,30),parent=self.rootNode)
             self.platz1a=avg.WordsNode (pos=(0,0),font="arial", variant="Bold", text="1. ", color="DDDC3C", fontsize=30, parent=self.div1)
             self.platz1b=avg.WordsNode (pos=(33,40),font="arial", variant="Bold", text="1. ", color="DDDC3C", fontsize=20, parent=self.div1)
             self.platz1c=avg.WordsNode (pos=(561,0),font="arial", variant="Bold", text="1. ", color="DDDC3C", fontsize=30, parent=self.div1)
+            
             
             self.div2=avg.DivNode(pos=(75,215), size=(3*(a/5)-20,30),parent=self.rootNode)
             self.platz2a=avg.WordsNode (pos=(0,0),font="arial", variant="Bold", text="2. ", color="C9C9C5", fontsize=30, parent=self.div2)
@@ -101,23 +102,18 @@ class screen(AVGApp):
             self.dritterName.text=leute[2][0]
             
             
-        def receiveArraywithSongs():
+        def receiveArraywithSongs(): ## Initialisieren
             
-            title=[]
-            title.append("Silbermond##Nichts passiert##7")
-            title.append("Juli##Gute Zeit##6")
-            title.append("Nickelback##Silver side up##5")
-            title.append("Carly Rae Jeapson##I just met you##4")
-            title.append("Sportfreunde Stiller##Applaus Applaus##3")
-            title.append("Will.I.am##Scream and Shout##2")
-            title.append("Justin Timberlake##Mirrors##1")
+            a="Silbermond##Nichts passiert##7!#!Juli##Gute Zeit##6!#!Nickelback##Silver side up##5!#!Citizens##True Romance##4!#!Sportfreunde Stiller##Applaus Applaus##3!#!Will.I.am##Scream and Shout##2!#!Justin Timberlake##Mirrors##1" 
+            songinput = a.split("!#!")
             stringarray=[]
 
-            ArrayLen = len(title)
+            ArrayLen = len(songinput)
             for i in range(0,ArrayLen):
-                string = title[i]
+                string = songinput[i]
                 string2 = string.split("##")
                 stringarray.append([string2[0],string2[1],string2[2]]) ##Interpret , Titel, Votes
+            print stringarray
                    
             
             self.platz1a.text= stringarray[0][1]
@@ -127,7 +123,7 @@ class screen(AVGApp):
             self.platz5a.text= stringarray[4][1]
             self.platz6a.text= stringarray[5][1]
             self.platz7a.text= stringarray[6][1]
-            
+              
             self.platz1b.text= stringarray[0][0]
             self.platz2b.text= stringarray[1][0]
             self.platz3b.text= stringarray[2][0]
@@ -135,7 +131,7 @@ class screen(AVGApp):
             self.platz5b.text= stringarray[4][0]
             self.platz6b.text= stringarray[5][0]
             self.platz7b.text= stringarray[6][0]
-            
+              
             self.platz1c.text= stringarray[0][2]
             self.platz2c.text= stringarray[1][2]
             self.platz3c.text= stringarray[2][2]
@@ -143,6 +139,18 @@ class screen(AVGApp):
             self.platz5c.text= stringarray[4][2]
             self.platz6c.text= stringarray[5][2]
             self.platz7c.text= stringarray[6][2]
+            
+        
+        def checkArraysthenchange(array1, array2):
+
+            ArrayLen = len(array1)
+            for i in range(0,ArrayLen):
+                string = songinput[i]
+                string2 = string.split("##")
+                stringarray.append([string2[0],string2[1],string2[2]]) ##Interpret , Titel, Votes
+            print stringarray
+            
+            
             
 
             
@@ -175,6 +183,7 @@ class screen(AVGApp):
         thread.start_new_thread(countdown,(0,2))
         receiveArraywithSongs()
         Tauschen(self.div1, self.div2, self.div1.x, self.div1.y, self.div2.x , self.div2.y)
+    
         
         
         
