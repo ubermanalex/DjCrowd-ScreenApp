4794c523746e68157eb72cbb77f3ce42eb28b2cd
Kirstin <Kirstin.Buchholz@web.de>
2013-06-19 15:35:21 +0200

    Main + Balken jetzt wirklich

--- main.py
+++ main.py
@@ -75,33 +75,254 @@ class screen(AVGApp):
                 animList = (animObj2, animObj1)
                 animObj = ParallelAnim(animList)
 
-                player.setTimeout(2000, startAnim)      
+                player.setTimeout(2000, startAnim) 
+        
+        def TauschenDIV(a,b,AX,BX):
+                def startAnim():
+                    animObj.start()
+        
+                animObj1 = LinearAnim(a, "x", 2000, AX, BX)
+                animObj2 = LinearAnim(b, "x", 2000, BX, AX)
+                animList = (animObj2, animObj1)
+                animObj = ParallelAnim(animList)
+
+                player.setTimeout(0, startAnim) 
+        
+        def Suchen(array, name):
+                i = 0
+                while i<2 :
+                    if array[i][0] == name:
+                        return i
+                    
+                
+                    i += 1
+                return -1
+            
+        def TauschenimArray(array, Position1, Position2):
+            name1 = array[Position1][0]
+            punkte1 = array[Position1][1]
+            array[Position1][0] = array[Position2][0]
+            array[Position1][1] = array[Position2][1]
+            array[Position2][0] = name1
+            array[Position2][1] = punkte1
+            
+        def SetzenimArray(array, name, punkte):
+            array[2][0] = name
+            array[2][1] = punkte     
             
             
         def right(): #rechts mit Balken
             self.divNode=avg.DivNode(pos=(a-2*(a/5),50), size=(2*(a/5),b-50),parent=self.rootNode)
             self.rightr=avg.RectNode (pos=(0,0), size=(2*(a/5), b-50), parent=self.divNode, color="F0F0F4", fillopacity=1)
             
+            breite = 2*(a/5)
             #On Start : 
-            leute=[]
-            leute.append(["Alexander", "0"])
-            leute.append(["Pascal", "0"])
-            leute.append(["Antonio", "0"])
+            self.leute=[]
+            self.leute.append(["Alexander", "0"])
+            self.leute.append(["Pascal", "0"])
+            self.leute.append(["Antonio", "0"])
             
-            self.erster=avg.RectNode(pos=(50,b-200), size=(30,5), parent=self.divNode, color="0489B1", fillcolor="2E9AFE", fillopacity=1)
-            self.zweiter=avg.RectNode(pos=(200,b-200), size=(30,5), parent=self.divNode, color="0489B1", fillcolor="2E9AFE", fillopacity=1)
-            self.zweiter=avg.RectNode(pos=(350, b-200), size=(30,5), parent=self.divNode, color="0489B1", fillcolor="2E9AFE",fillopacity=1)
+            self.divNode1=avg.DivNode(pos=(0,0), size=((breite/3),b-50),parent=self.divNode)
+            self.erster=avg.RectNode(pos=(50,b-155), size=(30,5), parent=self.divNode1, color="0489B1", fillcolor="2E9AFE", fillopacity=1)
+            self.ersterName=avg.WordsNode(pos=(50,b-100), text=" " ,parent=self.divNode1, font='arial', color="6E6E6E", fontsize=20)
+            self.ersterName.text=self.leute[0][0]
             
-            self.ersterName=avg.WordsNode(pos=(50,b-140), text=" " ,parent=self.divNode, font='arial', color="6E6E6E", fontsize=20)
-            self.ersterName.text=leute[0][0]
+            self.divNode2=avg.DivNode(pos=((breite/3),0), size=((breite/3),b-50),parent=self.divNode)
+            self.zweiter=avg.RectNode(pos=(50,b-155), size=(30,5), parent=self.divNode2, color="0489B1", fillcolor="2E9AFE", fillopacity=1)
+            self.zweiterName=avg.WordsNode(pos=(50,b-100), text=" " ,parent=self.divNode2, font='arial', color="6E6E6E", fontsize=20)
+            self.zweiterName.text=self.leute[1][0]
             
-            self.zweiterName=avg.WordsNode(pos=(200,b-140), text=" " ,parent=self.divNode, font='arial', color="6E6E6E", fontsize=20)
-            self.zweiterName.text=leute[1][0]
+            self.divNode3=avg.DivNode(pos=((breite-(breite/3)),0), size=((breite/3),b-50),parent=self.divNode)
+            self.dritter=avg.RectNode(pos=(50, b-155), size=(30,5), parent=self.divNode3, color="0489B1", fillcolor="2E9AFE",fillopacity=1)      
+            self.dritterName=avg.WordsNode(pos=(50,b-100), text=" " ,parent=self.divNode3, font='arial', color="6E6E6E", fontsize=20)
+            self.dritterName.text=self.leute[2][0]
             
-            self.dritterName=avg.WordsNode(pos=(350,b-140), text=" " ,parent=self.divNode, font='arial', color="6E6E6E", fontsize=20)
-            self.dritterName.text=leute[2][0]
+        def recievedpunkte(string,null):
+            
+            array = string.split("###")
+                         
+            neueLeute = []
+            neueLeute.append([array[1], array[2]])
+            neueLeute.append([array[3], array[4]])
+            neueLeute.append([array[5], array[6]])
             
             
+            PunkteErster = neueLeute[0][1]
+            NameErster = neueLeute[0][0]
+            PunkteErster = float(PunkteErster)
+            
+            PunkteZweiter = neueLeute[1][1]
+            NameZweiter = neueLeute[1][0]
+            PunkteZweiter = float(PunkteZweiter)
+            
+            PunkteDritter = neueLeute[2][1]
+            NameDritter = neueLeute[2][0]
+            PunkteDritter = float(PunkteDritter)
+            
+            Hundertprozent = b-200
+            Punktezweiter = (Hundertprozent/PunkteErster)*PunkteZweiter
+            Punktedritter = (Hundertprozent/PunkteErster)*PunkteDritter
+            
+            breite = 2*(a/5)
+            
+            
+            #Erster der neuen Liste nicht in alter Liste
+            if Suchen(self.leute, neueLeute[0][0])== -1 :
+                self.dritter.pos=(50,50)
+                self.dritter.size=(30, Hundertprozent)
+                self.dritterName.text=NameErster
+                
+                TauschenDIV(self.divNode2, self.divNode3, breite/3, breite-(breite/3))        
+                time.sleep(5)     
+                TauschenDIV(self.divNode3, self.divNode1, breite/3, 0)
+                
+                time.sleep(5)
+                
+                SetzenimArray(self.leute, neueLeute[0][0], neueLeute[0][1])
+                TauschenimArray(self.leute, 1, 2)                
+                TauschenimArray(self.leute, 0, 1)
+                
+                self.divNode1.pos = (0,0) 
+                self.erster.pos=(50,50) 
+                self.erster.size=(30,Hundertprozent)
+                self.ersterName.text=self.leute[0][0]
+            
+                self.divNode2.pos = (breite/3, 0)
+                self.zweiter.pos=(50,b-155)
+                self.zweiter.size=(30,5)
+                self.zweiterName.text=self.leute[1][0]
+            
+                self.divNode3.pos = (breite-(breite/3),0)
+                self.dritter.pos=(50,b-155)      
+                self.dritter.size=(30,5)
+                self.dritterName.text=self.leute[2][0]
+                
+            #Erster schon in der Liste 
+            else :
+                i = Suchen(self.leute, neueLeute[0][0])
+                if i == 2:
+                    self.dritter.pos=(50,50)
+                    self.dritter.size=(30,Hundertprozent)
+                    TauschenDIV(self.divNode2, self.divNode3, breite/3, breite-(breite/3))     
+                    time.sleep(5)           
+                    TauschenDIV(self.divNode3, self.divNode1, breite/3, 0)
+                    self.leute[2][1] = neueLeute[2][1]
+                    TauschenimArray(self.leute, 1, 2)                
+                    TauschenimArray(self.leute, 0, 1)
+                    
+                    time.sleep(5)
+                    
+                    self.divNode1.pos = (0,0) 
+                    self.erster.pos=(50,50) 
+                    self.erster.size=(30,Hundertprozent)
+                    self.ersterName.text=self.leute[0][0]
+            
+                    self.divNode2.pos = (breite/3, 0)
+                    self.zweiter.pos=(50,b-155)
+                    self.zweiter.size=(30,5)
+                    self.zweiterName.text=self.leute[1][0]
+            
+                    self.divNode3.pos = (breite-(breite/3),0)
+                    self.dritter.pos=(50,b-155)      
+                    self.dritter.size=(30,5)
+                    self.dritterName.text=self.leute[2][0]
+                    
+                    
+                elif i ==1:
+                    self.zweiter.pos=(50,50)
+                    self.zweiter.size=(30,Hundertprozent)
+                    TauschenDIV(self.divNode2, self.divNode1, breite/3, 0)
+                    self.leute[1][1] = neueLeute[1][1]
+                    TauschenimArray(self.leute, 0, 1)
+                    
+                    time.sleep(5)
+                    
+                    self.divNode1.pos = (0,0) 
+                    self.erster.pos=(50,50) 
+                    self.erster.size=(30,Hundertprozent)
+                    self.ersterName.text=self.leute[0][0]
+            
+                    self.divNode2.pos = (breite/3, 0)
+                    self.zweiter.pos=(50,b-155)
+                    self.zweiter.size=(30,5)
+                    self.zweiterName.text=self.leute[1][0]
+                    
+                    
+                else :
+                    self.erster.pos=(50,50)
+                    self.erster.size=(30,Hundertprozent)
+                    self.leute[0][1] = neueLeute[0][1]
+                    
+                    
+            time.sleep(2)
+            
+            #Zwite Person noch nicht in der Liste
+            if Suchen(self.leute, neueLeute[1][0])== -1 :
+                
+                self.dritter.pos= (50,50+(b-200)-Punktezweiter)
+                self.dritter.size=(30,Punktezweiter)
+                self.dritterName.text=NameZweiter
+                
+                TauschenDIV(self.divNode3, self.divNode2, breite-(breite/3), breite/3)
+                
+                SetzenimArray(self.leute, neueLeute[1][0], neueLeute[1][1])   
+                TauschenimArray(self.leute, 1, 2)
+                
+                time.sleep(5)
+            
+                self.divNode2.pos = (breite/3, 0)
+                self.zweiter.pos=(50,50+(b-200)-Punktezweiter)
+                self.zweiter.size=(30,Punktezweiter)
+                self.zweiterName.text=self.leute[1][0]
+            
+                self.divNode3.pos = (breite-(breite/3),0)
+                self.dritter.pos=(50,b-155)      
+                self.dritter.size=(30,5)
+                self.dritterName.text=self.leute[2][0]
+                
+                
+            #Zweite Person schon in Liste      
+            else :
+                i = Suchen(self.leute, neueLeute[1][0])
+                if i == 2:
+                    self.dritter.pos=(50,50+(b-200)-Punktezweiter)
+                    self.dritter.size=(30,Punktezweiter)
+                    TauschenDIV(self.divNode2, self.divNode3, breite/3, breite-(breite/3))
+                    self.leute[2][1] = neueLeute[2][1]
+                    TauschenimArray(self.leute, 1, 2)       
+                    
+                    time.sleep(5)
+            
+                    self.divNode2.pos = (breite/3, 0)
+                    self.zweiter.pos=(50,50+(b-200)-Punktezweiter)
+                    self.zweiter.size=(30,Punktezweiter)
+                    self.zweiterName.text=self.leute[1][0]
+            
+                    self.divNode3.pos = (breite-(breite/3),0)
+                    self.dritter.pos=(50,b-155)      
+                    self.dritter.size=(30,5)
+                    self.dritterName.text=self.leute[2][0] 
+                    
+                else:
+                    self.zweiter.pos=(50,50+(b-200)-Punktezweiter)
+                    self.zweiter.size=(30,Punktezweiter)
+                    self.leute[1][1] = neueLeute[1][1]
+                    
+            time.sleep(2)
+            #Dritter noch nicht in Liste
+            if Suchen(self.leute, neueLeute[2][0])== -1 :
+                
+                self.dritter.pos= (50,50+(b-200)-Punktedritter)
+                self.dritter.size=(30,Punktedritter)
+                self.dritterName.text=NameDritter
+                
+                SetzenimArray(self.leute, neueLeute[2][0], neueLeute[2][1])   
+            #Dritter in Liste
+            else:
+                self.dritter.pos= (50,50+(b-200)-Punktedritter)
+                self.dritter.size=(30,Punktedritter)
+                SetzenimArray(self.leute, neueLeute[2][0], neueLeute[2][1])   
+                
         def receiveArraywithSongs(): ## Initialisieren
             
             a="Silbermond##Nichts passiert##7!#!Juli##Gute Zeit##6!#!Nickelback##Silver side up##5!#!Citizens##True Romance##4!#!Sportfreunde Stiller##Applaus Applaus##3!#!Will.I.am##Scream and Shout##2!#!Justin Timberlake##Mirrors##1" 
@@ -181,6 +402,8 @@ class screen(AVGApp):
         left()
         right()
         thread.start_new_thread(countdown,(0,2))
+        string = ("Balken###Pascal###460###Alexander###210###Rebecca###60")
+        thread.start_new_thread(recievedpunkte,(string,0))
         receiveArraywithSongs()
         Tauschen(self.div1, self.div2, self.div1.x, self.div1.y, self.div2.x , self.div2.y)
     
@@ -190,6 +413,6 @@ class screen(AVGApp):
   
         
 if __name__=='__main__':
-    screen.start(resolution=(1440, 900))   
+    screen.start(resolution=(900, 600))   
   
 
